793bf93ae4dddcadbc42445cf60988f3ec239f7e
Mikhail Dubov <mdubov@gmail.com>
2014-03-18 12:15:00 +0000

    Displays solved sudoku

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require_relative './lib/grid'
+require_relative './lib/cell'
+
+
+
+def random_sudoku
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Grid.new(seed.join)
+  sudoku.solve
+  sudoku.to_s.chars
+end
 
 get '/' do
+  @current_solution = random_sudoku
   erb :index
 end
