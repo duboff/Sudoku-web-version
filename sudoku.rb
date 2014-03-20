f9eb91503fcb5ce643d93d68d4e9c4ea36cfb58f
Mikhail Dubov <mdubov@gmail.com>
2014-03-20 08:07:57 +0000

    Can play a game now, show solution

--- sudoku.rb
+++ sudoku.rb
@@ -1,25 +1,21 @@
 require 'sinatra'
 require 'sinatra/partial'
+require 'rack-flash'
 set :partial_template_engine, :erb
+use Rack::Flash
+set :session_secret, "I'm the secre key to sign the cookie"
 
 require_relative './lib/grid'
 require_relative './lib/cell'
-
-
-
 
 enable :sessions
 
 helpers do
   def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value == 0
+    must_be_guessed = puzzle_value.to_i == 0
     tried_to_guess = current_solution_value.to_i != 0
     guessed_incorrectly = current_solution_value != solution_value
-
-    if solution_to_check &&
-      must_be_guessed &&
-      tried_to_guess &&
-      guessed_incorrectly
+    if solution_to_check && tried_to_guess && guessed_incorrectly && must_be_guessed
       'incorrect'
     elsif !must_be_guessed
       'value-provided'
@@ -32,9 +28,6 @@ helpers do
 
 end
       
-
-
-
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
   sudoku = Grid.new(seed.join)
@@ -42,31 +35,49 @@ def random_sudoku
   sudoku.to_s.chars
 end
 
-def puzzle(sudoku)
-  sudoku
+def puzzle(sudoku, difficulty)
+  game = sudoku.clone
+  x = 30 if difficulty == :easy
+  x = 50 if difficulty == :hard
+  seed = (0...81).to_a.shuffle[0..x]
+  seed.each { |ind| game[ind] = '0'  }
+  game
 end
 
 def box_order_to_row_order(cells)
   boxes = cells.each_slice(9).to_a
-
+  (0..8).to_a.inject([]) do |memo, i| 
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box|
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+    memo += three_rows_of_three.flatten
+  end
 end
 
-def generate_new_puzzle_if_necessary
-  return if session[:current_solution] && session[:solution] && session[:puzzle]
+def generate_new_puzzle_if_necessary(difficulty)
+  return if session[:current_solution]
   sudoku = random_sudoku
   session[:solution] = sudoku
-  session[:puzzle] = puzzle(sudoku)
+  session[:puzzle] = puzzle(sudoku,difficulty)
   session[:current_solution] = session[:puzzle]
 end
 
 def prepare_to_check_solution
   @check_solution = session[:check_solution]
+  if @check_solution
+      flash[:notice] = "Incorrect values are highlighted in yellow"
+  end
   session[:check_solution] = nil
 end
 
 get '/' do
+  session[:difficulty] ||= :easy
   prepare_to_check_solution
-  generate_new_puzzle_if_necessary
+  generate_new_puzzle_if_necessary(session[:difficulty])
   @current_solution = session[:current_solution] || session[:puzzle]
   @solution = session[:solution]
   @puzzle = session[:puzzle]
@@ -74,18 +85,36 @@ get '/' do
 end
 
 post '/' do
-  cells = params["cell"]
+  cells = box_order_to_row_order(params["cell"])
   session[:current_solution] = cells.map {|value| value.to_i }.join
   session[:check_solution] = true
   redirect to('/')
 end
 
 
+post '/newgame' do
+  session[:current_solution] = nil
+  session[:difficulty] = params[:difficulty].to_sym
+  redirect to('/')
+end
+
 get '/last-visit' do
   "Previous visit to homepage: #{session[:last_visit]}"
 end
 
 get '/solution' do
+  redirect to('/') if !session[:solution]
   @current_solution = session[:solution]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
   erb :index
 end
+
+post '/savegame' do
+  cells = box_order_to_row_order(params["cell"])
+  session[:current_solution] = cells.map {|value| value.to_i }.join
+    redirect to('/')
+end
+
+
+
